685d1339b039e79405ee55c193838907a43e79fb
unknown <phamquyen22445@gmail.com>
2024-05-05 09:21:22 +0700

    first commit

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,13 @@
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export default function Page({ params }: Params) {
+  return <>
+  <h1>Slug: {params.slug}</h1>
+  <p>HoleTex</p>
+  </>
+  
+}
